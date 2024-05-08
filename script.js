c9fd03f80667b074cb50c45b9f62bac0d95b91da
Rizal Mohamad <rizalprasetyo739@gmail.com>
2024-05-08 09:07:45 +0700

    menambahkan fitur filter

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// =============== SIDEBAR ===============
 const body = document.querySelector("body"),
   sidebar = body.querySelector("nav"),
   toggle = body.querySelector(".toggle"),
@@ -6,15 +7,34 @@ const body = document.querySelector("body"),
   modeText = body.querySelector(".mode-text");
 
 toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
+  sidebar.classList.toggle("close-sidebar");
 });
 
+// ============= Darkmode
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
-
   if (body.classList.contains("dark")) {
     modeText.innerText = "Light mode";
   } else {
     modeText.innerText = "Dark mode";
   }
+});
+
+// =============== CONTENT DASHBOARD ===============
+
+// Function Dropdown Filter
+const selectBtns = document.querySelectorAll(".select-btn");
+
+selectBtns.forEach((selectBtn) => {
+  selectBtn.addEventListener("click", () => {
+    const isOpen = selectBtn.classList.toggle("open-list");
+    if (isOpen) {
+      const openDropdowns = document.querySelectorAll(".open-list");
+      openDropdowns.forEach((dropdown) => {
+        if (dropdown !== selectBtn) {
+          dropdown.classList.remove("open-list");
+        }
+      });
+    }
+  });
 });
