6a1ce7a9ebaa8dfee65f133364a8cb4811b9fd4e
Rizal Mohamad <rizalprasetyo739@gmail.com>
2024-05-18 16:25:16 +0700

    add filter pizza type using fetch() from data.json

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,6 +1,5 @@
-// =============== CONTENT DASHBOARD ===============
-// =============== CONTENT DASHBOARD ===============
-
+// =============== FILTER DASHBOARD ===============
+// =============== FILTER DASHBOARD ===============
 const selectBtns = document.querySelectorAll(".select-btn");
 
 selectBtns.forEach((selectBtn) => {
@@ -16,3 +15,27 @@ selectBtns.forEach((selectBtn) => {
     }
   });
 });
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const pizzaList = document.querySelector(".list-items");
+    const pizzas = [];
+    console.log(data)
+
+    data.forEach((item) => {
+      const pizza = item["Pizza Type ID"];
+      if (pizzas.indexOf(pizza) === -1) {
+        pizzas.push(pizza);
+        console.log(pizza)
+
+        const listPizza = document.createElement("li");
+        listPizza.className = "item";
+        listPizza.innerHTML = `
+                <input type="checkbox" id="${pizza}" />
+                <label for="${pizza}">${pizza}</label>
+            `;
+        pizzaList.appendChild(listPizza);
+      }
+    });
+  });
