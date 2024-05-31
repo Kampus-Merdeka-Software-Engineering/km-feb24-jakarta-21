52775a86b039ea295df06a9f6b1c57b302cfb138
Rizal Mohamad <rizalprasetyo739@gmail.com>
2024-05-31 23:29:44 +0700

    update profile & sidebar.js

--- javascript/profile.js
+++ javascript/profile.js
@@ -2,68 +2,68 @@ let teamName = [
   {
     image: "../asset/levi.webp",
     name: "Ghaniya Madinah Kesrul",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Project Leader", "Project Leader"],
+    universitas: "Universitas Al-Ghifari",
+    jobdesk: ["DA Project Leader", " "],
   },
   {
     image: "../asset/levi.webp",
     name: "Theresia Elvita Tjia",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Analysis", "Deployment Team"],
+    universitas: "Universitas Kampus Merdeka",
+    jobdesk: ["DA Data Analysis", "SE Deployment Team"],
   },
   {
     image: "../asset/levi.webp",
     name: "Siti Maysaroh Zain",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Cleaning", "Frontend Developer"],
+    universitas: "Universitas Kampus Merdeka",
+    jobdesk: ["DA Data Cleaning", "SE Frontend Developer"],
   },
   {
     image: "../asset/levi.webp",
     name: "Nur Hikmah",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Visualization", "Quality Assurance"],
+    universitas: "Universitas Kampus Merdeka",
+    jobdesk: ["DA Data Visualization", "SE Quality Assurance"],
   },
   {
     image: "../asset/levi.webp",
     name: "Rafif Imam Faisal",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Communication", "Frontend Developer"],
+    universitas: "Universitas Dian Nuswantoro",
+    jobdesk: ["DA Data Communication", "SE Frontend Developer"],
   },
   {
     image: "../asset/levi.webp",
     name: "Mohamad Rizal Prasetyo",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Cleaning", "Frontend Developer"],
+    universitas: "Universitas Insan Pembangunan",
+    jobdesk: ["DA Data Cleaning", "SE Frontend Developer"],
   },
   {
     image: "../asset/levi.webp",
     name: "Ni Nyoman Kania Amandari",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Analysis", "Quality Assurance"],
+    universitas: "Universitas Kampus Merdeka",
+    jobdesk: ["DA Data Analysis", "SE Quality Assurance"],
   },
   {
     image: "../asset/levi.webp",
     name: "Syifa Amalia Putri",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Visualization", "Pitch Deck Team"],
+    universitas: "Universitas Kampus Merdeka",
+    jobdesk: ["DA Data Visualization", "SE Pitch Deck Team"],
   },
   {
     image: "../asset/levi.webp",
     name: "Agung Hidayat",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Comunication", "Pitch Deck Team"],
+    universitas: "Politeknik Negri Semarang",
+    jobdesk: ["DA Data Comunication", "SE Pitch Deck Team"],
   },
   {
     image: "../asset/levi.webp",
     name: "Kentji Hadiwidjajalu",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Analysis", "Quality Assurance"],
+    universitas: "Universitas Kampus Merdeka",
+    jobdesk: ["DA Data Analysis", "SE Quality Assurance"],
   },
   {
     image: "../asset/levi.webp",
     name: "Luther Jonatan Samuri",
-    universitas: "Universitas Kampus Merdeka 2024",
-    jobdesk: ["Data Visualization", "Frontend Developer"],
+    universitas: "Universitas Kampus Merdeka",
+    jobdesk: ["DA Data Visualization", "SE Frontend Developer"],
   },
 ];
 
@@ -76,11 +76,11 @@ for (let i in teamName) {
     </div>
     <div class="card-body">
       <h1 class="name">${teamName[i].name}</h1>
-      <p class="name-univ">${teamName[i].universitas}</p>
       <ul class="name-job">
         <li>${teamName[i].jobdesk[0]}</li>
         <li>${teamName[i].jobdesk[1]}</li>
       </ul>
+      <p class="name-univ">${teamName[i].universitas}</p>
     </div>
     </div>
     `;
