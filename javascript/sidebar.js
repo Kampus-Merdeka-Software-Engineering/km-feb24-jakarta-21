4d9bf9c1efddca9540cf42352ac3b701e509a23b
Rizal Mohamad <rizalprasetyo739@gmail.com>
2024-06-03 12:00:49 +0700

    upadate % add animasi in sidebar

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -5,31 +5,27 @@ const toggle = body.querySelector(".toggle");
 const hamburgerMenu = body.querySelector(".nav-hamburger");
 const modeSwitch = body.querySelector(".toggle-switch");
 const modeText = body.querySelector(".mode-text");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
 
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close-sidebar");
 });
 hamburgerMenu.addEventListener("click", () => {
   sidebar.classList.toggle("close-sidebar");
+  toggleMenuIcon()
 });
 
-// ===== Darkmode
-// let getMode = localStorage.getItem("mode");
-// if (getMode && getMode === "dark") {
-//   body.classList.toggle("dark");
-//   modeSwitch.classList.toggle("active");
-// }
-
-// modeSwitch.addEventListener("click", () => {
-//   body.classList.toggle("dark");
-//   if (!body.classList.contains("dark")) {
-//     modeText.innerText = "Dark Mode";
-//     return localStorage.setItem("mode", "light");
-//   }
-//   modeText.innerText = "Light Mode";
-//   localStorage.setItem("mode", "dark");
-// });
-// modeSwitch.addEventListener("click", () => modeSwitch.classList.toggle("active"));
+function toggleMenuIcon() {
+  if (sidebar.classList.contains("close-sidebar")) {
+    openMenu.style.display = "block";
+    closeMenu.style.display = "none"; 
+  } else {
+    openMenu.style.display = "none";
+    closeMenu.style.display = "block"; 
+  }
+}
+toggleMenuIcon()
 
 // ============ Darkmode =============
 let getMode = localStorage.getItem("mode");
